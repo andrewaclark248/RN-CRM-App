331bd38231b16613f7e1935d45099910c6b3816e
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-03 22:42:27 -0500

    add navbar

---
+++ components/SelectRegion.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+
+export default function SelectRegion() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Select Region!</Text>
+      </View>
+    );
+  }
+  
