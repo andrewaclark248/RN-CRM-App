9c43f505725b1994a2ff4d5755b339a1ede21c40
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-07 17:12:10 -0500

    various changes

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
 import Home from './components/Home.js';
 import SelectRegion from './components/SelectRegion';
-const Tab = createBottomTabNavigator();
 
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="SelectRegion" component={SelectRegion} />
-        </Tab.Navigator>
-      </NavigationContainer>
-  );
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>  
+    );
 }
 
 const styles = StyleSheet.create({
