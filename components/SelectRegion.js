4c30823a8264411211ff07efbd4487d47c8110d6
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-07 18:03:51 -0500

    add dropdown

--- components/SelectRegion.js
+++ components/SelectRegion.js
@@ -1,11 +1,73 @@
 import { View, Text } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 
 export default function SelectRegion() {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+  ];
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Select Region!</Text>
+        <Dropdown
+          style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+          data={data}
+          labelField="label"
+          valueField="value"
+          onChange={() => console.log("some log")}
+          onChangeText={() => console.log("some log")}
+        />
       </View>
     );
   }
   
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      padding: 16,
+    },
+    dropdown: {
+      width: 150,
+      height: 50,
+      borderColor: 'gray',
+      borderWidth: 0.5,
+      borderRadius: 8,
+      paddingHorizontal: 8,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    label: {
+      position: 'absolute',
+      backgroundColor: 'white',
+      left: 22,
+      top: 8,
+      zIndex: 999,
+      paddingHorizontal: 8,
+      fontSize: 14,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
