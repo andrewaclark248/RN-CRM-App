2d0637620cdcd23c39fef5528839d948baa49bb6
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-06 20:14:14 -0500

    add dropdown

--- components/SelectRegion.js
+++ components/SelectRegion.js
@@ -1,20 +1,30 @@
 import { View, Text } from 'react-native';
-//import { Dropdown } from 'react-native-material-dropdown';
+import { SelectList } from 'react-native-dropdown-select-list'
+import React, { useState } from 'react';
 
 
 export default function SelectRegion() {
-    let data = [{
-        value: 'Banana',
-      }, {
-        value: 'Mango',
-      }, {
-        value: 'Pear',
-      }];
-   
+    const [selected, setSelected] = useState("");
+  
+    const data = [
+        {key:'1', value:'Mobiles', disabled:true},
+        {key:'2', value:'Appliances'},
+        {key:'3', value:'Cameras'},
+        {key:'4', value:'Computers', disabled:true},
+        {key:'5', value:'Vegetables'},
+        {key:'6', value:'Diary Products'},
+        {key:'7', value:'Drinks'},
+    ]
+  
     return (
+        
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Select Region!</Text>
-
+        <SelectList 
+            setSelected={(val) => setSelected(val)} 
+            data={data} 
+            save="value"
+        />
       </View>
     );
   }
