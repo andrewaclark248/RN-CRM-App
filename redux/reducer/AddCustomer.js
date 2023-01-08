db856810550fcc54dc942a8450c32522b06f3f19
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-08 18:37:41 -0500

    add redu for customer

---
+++ redux/reducer/AddCustomer.js
@@ -0,0 +1,25 @@
+import { ADD_CUSTOMER } from '../actions/addCustomer' //redux action
+import { STORE_CUSTOMER } from '../../async_storage_data/index' //async action
+import { storeData } from '../../async_storage_data/AsyncData'
+
+
+const initialState = {
+	customers: []
+}
+
+export const updateRegion = (state = initialState, action) => {
+    let returnVar = null;
+    if (action.type == ADD_CUSTOMER) {
+        var customers = state.customers.concat(action.payload.customer)
+
+        storeData(STORE_CUSTOMER, JSON.stringify(customers))
+        returnVar = {
+            ...state,
+            customers: customers
+        }
+        
+    } else {
+        returnVar = state;
+    }
+    return returnVar;
+}
