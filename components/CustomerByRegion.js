7574d32ca14a3d753138d5a9293c3ec22140d837
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2023-01-08 19:32:18 -0500

    Get Customer By Region (#6)
    * add paper
    
    * add customers by region

--- components/CustomerByRegion.js
+++ components/CustomerByRegion.js
@@ -1,14 +1,39 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { connect } from "react-redux";
+import { DataTable } from 'react-native-paper';
 
 
 function CustomerByRegion(props) {
-    var customersInRegion = props.customers.filter((customer) => {
+    let customersInRegion = props.customers.filter((customer) => {
       return customer.region == props.region
     })
+    console.log(props.region)
+    console.log("customers in region === ", customersInRegion)
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Customer By Region!</Text>
+        <DataTable style={styles.container}>
+          <DataTable.Header style={styles.tableHeader}>
+            <DataTable.Title>First Name</DataTable.Title>
+            <DataTable.Title>Last Name</DataTable.Title>
+            <DataTable.Title>Region</DataTable.Title>
+            <DataTable.Title>Status</DataTable.Title>
+          </DataTable.Header>
+            {customersInRegion.map((customer, index) => {
+              {return(
+                <DataTable.Row key={index}>
+                  <DataTable.Cell>{ customer.firstName }</DataTable.Cell>
+                  <DataTable.Cell>{ customer.lastName }</DataTable.Cell>
+                  <DataTable.Cell>{ customer.region }</DataTable.Cell>
+                  <DataTable.Cell>{ customer.status }</DataTable.Cell>
+                </DataTable.Row>
+              )}
+
+            })
+            }
+          
+
+        </DataTable> 
       </View>
     );
   }
@@ -18,3 +43,12 @@ function CustomerByRegion(props) {
     region: state.regionReducer.region,
     customers: state.customerReducer.customers
   }), null)(CustomerByRegion);
+
+  const styles = StyleSheet.create({
+    container: {
+      padding: 15,
+    },
+    tableHeader: {
+      backgroundColor: '#DCDCDC',
+    },
+  });
