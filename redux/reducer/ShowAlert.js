db5b89077d5b43ea0f5282e1b4eacf9869613b27
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2023-01-14 10:50:18 -0500

    fix alert bug (#10)

---
+++ redux/reducer/ShowAlert.js
@@ -0,0 +1,20 @@
+import { SHOW_CREATED_CUSTOMER_ALERT } from '../actions/showAlertAction.js'
+
+
+const initialState = {
+	showCreatedCustomerAlert: false
+}
+
+export const showAlertReducer = (state = initialState, action) => {
+    let returnVar = null;
+    if (action.type == SHOW_CREATED_CUSTOMER_ALERT) {
+
+        returnVar = {
+            ...state,
+            showCreatedCustomerAlert: !state.showCreatedCustomerAlert
+        }
+    } else {
+        returnVar = state;
+    }
+    return returnVar;
+}
--- redux/reducer/rootReducer.js
+++ redux/reducer/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 import { updateRegion } from './AddUpdateRegion'
 import { addCustomer } from './AddCustomer'
 import { asyncStorageReducer } from './AsyncStorageReducer'
+import { showAlertReducer } from './ShowAlert'
 
 
 
 const rootReducer = combineReducers({
 	regionReducer: updateRegion,
 	customerReducer: addCustomer,
-	asyncStorageReducer: asyncStorageReducer
+	asyncStorageReducer: asyncStorageReducer,
+	showAlertReducer: showAlertReducer
 })
 
 export default rootReducer;
